64f32d8589f36b50dc31507ac16eb7650f48d4e5
jonaysalas <36030913+jonaysalas@users.noreply.github.com>
2025-03-27 16:16:38 +0100

    Actualización de OPR-Spanier
    Como la lista de conjuros solo aparece si el ejercito tiene Casters, ya no se despliega la lista de conjuro para que no se traduzca.
    Se usa ahora a MyMemoryTranslator porque traduce mejor que GoogleTranslate (roll -> tirada, por ejem)
    Para reducir el uso del translator, ahora se guardan las traducciones en un diccionario y se vuelca a un TXT para poder cargarlo en futuras ejecuciones. Ha resultado mejorar mucho el funcionamiento del programa.

--- webWorker.py
+++ webWorker.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from time import sleep
 import base64
-from deep_translator import GoogleTranslator
+from deep_translator import MyMemoryTranslator
 
 manualTranslator = {}
 f = open('manualTranslator.txt', 'r', encoding='utf-8')
@@ -14,7 +14,15 @@ for line in f.readlines():
     manualTranslator[data[0]+":"] = data[1].strip()+":"
 f.close()
 
-translator = GoogleTranslator(source='en', target='es')
+prevTranslator = {}
+f = open('translatorHistoryv341.txt', 'r', encoding='utf-8')
+for line in f.readlines():
+    data = line.split('\t')
+    if data[1].strip()[-2:] == "::": continue
+    prevTranslator[data[0]] = data[1].strip()
+f.close()
+
+translator = MyMemoryTranslator(source='english', target='spanish')
 options = Options()
 options.add_argument('-headless')
 driver = webdriver.Firefox(options)
@@ -23,37 +31,47 @@ sleep(3)
 driver.find_element(By.XPATH, "//div[@class='MuiStack-root css-u4p24i']/button[1]").click()
 f = open('ListLinks.txt','r')
 
-firstLoop = True
 for lin in f.readlines():
-    fileName = lin.split("name=")[1][:-1]
-    driver.get(lin)
-    sleep(5)
-    if firstLoop:
-        driver.find_element(By.XPATH, "//div[@class='MuiStack-root no-print css-1mhrztx']/button[2]").click()
-        sleep(1)
-        driver.find_element(By.XPATH, "//li[@class='MuiListItem-root MuiListItem-gutters MuiListItem-padding css-j4r5fq']/span[1]").click()
-        sleep(1)
-        driver.find_element(By.XPATH, "//div[@class='MuiStack-root css-1xctuv5']/button[1]").click()
-        firstLoop = False
-    elems = driver.find_elements(By.XPATH, "//div[@class='MuiContainer-root css-2og7kz']//span")
-    #elems += driver.find_elements(By.XPATH, "//div[@class='MuiContainer-root css-2og7kz']//td")
-    for i in elems:
-        try:
-            orig = i.text
-            if orig == None: continue
-            if orig.strip() in list(manualTranslator.keys()):
-                trans = orig.replace(orig.strip(), manualTranslator[orig.strip()])
+    try:
+        fileName = lin.split("name=")[1][:-1]
+        fileName = fileName.replace("%20", "_")
+        driver.get(lin)
+        sleep(5)
+        elems = driver.find_elements(By.XPATH, "//div[@class='MuiContainer-root css-2og7kz']//span")
+        #elems += driver.find_elements(By.XPATH, "//div[@class='MuiContainer-root css-2og7kz']//td")
+        for i in elems:
+            try:
+                orig = i.text
+                if orig == None: continue
+                elif orig.strip()[-1] == '+': continue
+                if orig.strip() in list(manualTranslator.keys()):
+                    trans = orig.replace(orig.strip(), manualTranslator[orig.strip()])
+                elif orig.strip() in list(prevTranslator.keys()):
+                    trans = orig.replace(orig.strip(), prevTranslator[orig.strip()])
+                else:
+                    trans = orig.replace(orig.strip(), translator.translate(orig.strip()))
+                    prevTranslator[orig.strip()] = trans
 
-            else:
-                trans = orig.replace(orig.strip(), translator.translate(text =orig.strip()))
-            driver.execute_script("arguments[0].innerText = '{}'".format(trans), i)
-        except Exception as e:
-            print(e)
-            None
+                driver.execute_script("arguments[0].innerText = '{}'".format(trans), i)
+            except Exception as e:
+                print(e)
+                None
 
-    pdf = driver.print_page()
-    with open('{}.pdf'.format(fileName), 'wb') as file:
-        file.write(base64.b64decode(pdf))
+        sleep(5)
+        pdf = driver.print_page()
+        with open('{}.pdf'.format(fileName), 'wb') as file:
+            file.write(base64.b64decode(pdf))
+        
+        print("The {} army list generated in pdf".format(fileName))
+    except:
+        continue
 
+f.close()
+
+f = open('translatorHistoryv341.txt', 'w', encoding='utf-8')
+txt = ""
+for key in list(prevTranslator.keys()):
+    txt += "{}\t{}\n".format(key, prevTranslator[key])
+f.write(txt[:-1]) #we remove the last '\n'
 f.close()
 driver.close()
