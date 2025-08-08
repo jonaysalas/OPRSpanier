414941016ba87c3becd856706acf84b32309c725
jonaysalas <36030913+jonaysalas@users.noreply.github.com>
2025-08-08 13:09:11 +0200

    Actualiazación del OPR Spainer
    Se ha actualizado el código para reducir las consultas a la api de Google Translate.
    Se ha rellenado translatorHistory341.txt para reducir las consultas a la API, lo que ha hecho el "manualTranslator.txt" obsoleto
    Se ha retocado la herramienta para funcionar con la nueva web de ArmyForge

--- webWorker.py
+++ webWorker.py
@@ -2,10 +2,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.print_page_options import PrintOptions
 from time import sleep
 import base64
 from deep_translator import MyMemoryTranslator
 
+print("V1.0")
 manualTranslator = {}
 f = open('manualTranslator.txt', 'r', encoding='utf-8')
 for line in f.readlines():
@@ -17,6 +19,9 @@ f.close()
 prevTranslator = {}
 f = open('translatorHistoryv341.txt', 'r', encoding='utf-8')
 for line in f.readlines():
+    if "\t\n" in data:
+        continue
+    print(line)
     data = line.split('\t')
     if data[1].strip()[-2:] == "::": continue
     prevTranslator[data[0]] = data[1].strip()
@@ -28,7 +33,7 @@ options.add_argument('-headless')
 driver = webdriver.Firefox(options)
 driver.get("https://army-forge.onepagerules.com/")
 sleep(3)
-driver.find_element(By.XPATH, "//div[@class='MuiStack-root css-u4p24i']/button[1]").click()
+driver.find_element(By.XPATH, "//div[@class='MuiStack-root mui-u4p24i']/button[1]").click()
 f = open('ListLinks.txt','r')
 
 for lin in f.readlines():
@@ -37,7 +42,28 @@ for lin in f.readlines():
         fileName = fileName.replace("%20", "_")
         driver.get(lin)
         sleep(5)
-        elems = driver.find_elements(By.XPATH, "//div[@class='MuiContainer-root css-2og7kz']//span")
+        elems = driver.find_elements(By.TAG_NAME, "span")
+        #elems += driver.find_elements(By.XPATH, "//div[@class='MuiContainer-root css-2og7kz']//td")
+        for i in elems:
+            try:
+                orig = i.text
+                if orig == None: continue
+                elif orig.strip()[-1] == '+': continue
+                if orig.strip() in list(manualTranslator.keys()):
+                    trans = orig.replace(orig.strip(), manualTranslator[orig.strip()])
+                elif orig.strip() in list(prevTranslator.keys()):
+                    trans = orig.replace(orig.strip(), prevTranslator[orig.strip()])
+                else:
+                    trans = orig.replace(orig.strip(), translator.translate(orig.strip()))
+                    if len(trans) > 1:
+                        prevTranslator[orig.strip()] = trans
+
+                driver.execute_script("arguments[0].innerText = '{}'".format(trans), i)
+            except Exception as e:
+                print(e)
+                None
+        '''
+        elems = driver.find_elements(By.XPATH, "//div[@class='MuiBox-root mui-1v95bqz']//span")
         #elems += driver.find_elements(By.XPATH, "//div[@class='MuiContainer-root css-2og7kz']//td")
         for i in elems:
             try:
@@ -51,27 +77,32 @@ for lin in f.readlines():
                 else:
                     trans = orig.replace(orig.strip(), translator.translate(orig.strip()))
                     prevTranslator[orig.strip()] = trans
-
+                if ":" in trans:
+                    trans = trans.replace(":", ": ")
                 driver.execute_script("arguments[0].innerText = '{}'".format(trans), i)
             except Exception as e:
                 print(e)
                 None
-
+        '''
         sleep(5)
-        pdf = driver.print_page()
+        print_options = PrintOptions()
+        print_options.shrink_to_fit = True 
+        print_options.page_height = 29.7  # Use page_width to assign width
+        pdf = driver.print_page(print_options)
         with open('{}.pdf'.format(fileName), 'wb') as file:
             file.write(base64.b64decode(pdf))
         
         print("The {} army list generated in pdf".format(fileName))
-    except:
+    except Exception as e:
+        print(e)
         continue
-
 f.close()
 
 f = open('translatorHistoryv341.txt', 'w', encoding='utf-8')
 txt = ""
 for key in list(prevTranslator.keys()):
-    txt += "{}\t{}\n".format(key, prevTranslator[key])
+    if prevTranslator[key].strip() != "":
+        txt += "{}\t{}\n".format(key, prevTranslator[key])
 f.write(txt[:-1]) #we remove the last '\n'
 f.close()
 driver.close()
