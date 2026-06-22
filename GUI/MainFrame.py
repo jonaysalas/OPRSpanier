2d23617e31943912932259a3db6ac13050b131e8
jonaysalas <36030913+jonaysalas@users.noreply.github.com>
2026-06-22 10:37:18 +0200

    Actualizado el GUI
    MainFrame: Se han añadido más opciones al menú, como editar o crear ficheros traductor
    EditTranslator: Se ha añadido una ventana para poder editar los traductores de una forma más sencilla. Funciona para editar el traductor automatico, el traductor manual y para incluir nuevas traducciones al traductor manual
    MultilineTxtDialog: Dialogo para editar la tabla sin que se "oculte" el campo anterior o lo que se está escribiendo.

--- GUI/MainFrame.py
+++ GUI/MainFrame.py
@@ -11,6 +11,9 @@ import wx
 import wx.xrc
 import os
 from Core.webWorker import WebWorker
+import pickle
+from GUI.EditTranslator import TranslatorFrame
+from Core.GeneralMethods import SaveATranslatorDictionary
 
 import gettext
 _ = gettext.gettext
@@ -33,8 +36,26 @@ class MainFrame ( wx.Frame ):
 
         self.config_browserVisibility = wx.MenuItem( self.configMenu, wx.ID_ANY, _(u"Ocultar Navegador: ON"), wx.EmptyString, wx.ITEM_NORMAL )
         self.configMenu.Append( self.config_browserVisibility )
+        
+        self.config_SaveConfig = wx.MenuItem( self.configMenu, wx.ID_ANY, _(u"Guardar Configuración"), wx.EmptyString, wx.ITEM_NORMAL )
+        self.configMenu.Append( self.config_SaveConfig )
 
         self.m_menubar1.Append( self.configMenu, _(u"Config") )
+
+        self.menuTraductores = wx.Menu()
+        self.trad_TranslationHistory = wx.MenuItem( self.menuTraductores, wx.ID_ANY, _(u"Editar historial de Traducciones"), wx.EmptyString, wx.ITEM_NORMAL )
+        self.menuTraductores.Append( self.trad_TranslationHistory )
+
+        self.trad_ResetTranslations = wx.MenuItem( self.menuTraductores, wx.ID_ANY, _(u"Resetear historial de traducciones"), wx.EmptyString, wx.ITEM_NORMAL )
+        self.menuTraductores.Append( self.trad_ResetTranslations )
+
+        self.trad_CreateTransManual = wx.MenuItem( self.menuTraductores, wx.ID_ANY, _(u"Crear Traductor Manual"), wx.EmptyString, wx.ITEM_NORMAL )
+        self.menuTraductores.Append( self.trad_CreateTransManual )
+
+        self.trad_EditTransManual = wx.MenuItem( self.menuTraductores, wx.ID_ANY, _(u"Editar Traductor Manual"), wx.EmptyString, wx.ITEM_NORMAL )
+        self.menuTraductores.Append( self.trad_EditTransManual )
+
+        self.m_menubar1.Append( self.menuTraductores, _(u"Traductores") )
 
         self.SetMenuBar( self.m_menubar1 )
 
@@ -100,12 +121,18 @@ class MainFrame ( wx.Frame ):
         # Connect Events
         self.Bind( wx.EVT_MENU, self.ConfigureTranslator, id = self.config_dictionary.GetId() )
         self.Bind( wx.EVT_MENU, self.toogleBrowserVisibility, id = self.config_browserVisibility.GetId() )
+        self.Bind( wx.EVT_MENU, self.SaveConfiguration, id = self.config_SaveConfig.GetId() )
+        self.Bind( wx.EVT_MENU, self.EditTranslator, id = self.trad_TranslationHistory.GetId() )
+        self.Bind( wx.EVT_MENU, self.ResetTranslator, id = self.trad_ResetTranslations.GetId() )
+        self.Bind( wx.EVT_MENU, self.CreateManualTranslator, id = self.trad_CreateTransManual.GetId() )
+        self.Bind( wx.EVT_MENU, self.EditTranslatorManual, id = self.trad_EditTransManual.GetId() )
         self.btnGenerarPDFs.Bind( wx.EVT_BUTTON, self.startGeneration )
 
         # Useful Variables
         self.BrowserVisibility = False
         self.pathManualTranslator = ""
         self.pathTranslations = "config/translation/Translations.txt"
+        self.LoadConfiguration()
 
     def __del__( self ):
         pass
@@ -135,6 +162,23 @@ class MainFrame ( wx.Frame ):
             dialog = wx.MessageDialog(self, errors, style = wx.OK|wx.ICON_ERROR|wx.STAY_ON_TOP)
             dialog.ShowModal()
             dialog.Destroy()
+            return False
+        return True
+    
+    def LoadConfiguration(self):
+        if os.path.exists("config/configuration.txt"):
+            file = open("config/configuration.txt", 'rb')
+            try: 
+                self.BrowserVisibility = pickle.load(file)
+                self.toogleBrowserVisibility(None)
+
+                self.pathManualTranslator = pickle.load(file)
+
+                self.filePath_ListasURL.SetPath(pickle.load(file))
+                self.dirPdfs.SetPath(pickle.load(file))
+            except:
+                None
+    
 
     # Virtual event handlers, override them in your derived class
     def ConfigureTranslator( self, event ):
@@ -144,9 +188,69 @@ class MainFrame ( wx.Frame ):
         dialog.Destroy()
 
     def toogleBrowserVisibility( self, event ):
-        self.BrowserVisibility = not self.BrowserVisibility
+        if event != None: #There is an event, so just verify the text is correct
+            self.BrowserVisibility = not self.BrowserVisibility
 
         self.config_browserVisibility.SetItemLabel("Ocultar Navegador: {}".format('OFF' if self.BrowserVisibility else 'ON'))
+    
+    def SaveConfiguration(self, event):
+        file = open('config/configuration.txt', 'wb')
+        pickle.dump(self.BrowserVisibility, file)
+        pickle.dump(self.pathManualTranslator, file)
+        pickle.dump(self.filePath_ListasURL.GetPath(), file)
+        pickle.dump(self.dirPdfs.GetPath(), file)
+
+    def EditTranslator( self, event ):
+        tf = TranslatorFrame(self)
+        tf.ConfigureForHistory(self.pathTranslations)
+        self.Hide()
+        tf.Show()
+
+    def ResetTranslator( self, event ):
+        message = "Se eliminarán todas las traducciones previas del traductor\n"
+        message += "Tenga en cuenta que la API para traducir automáticamente tiene un limite de usos al día\n"
+        message += "¿Desea proceder?"
+
+        dialog =  wx.MessageDialog(self, message, "Aviso al usuario", style = wx.YES_NO|wx.ICON_WARNING|wx.STAY_ON_TOP)
+        if dialog.ShowModal() == wx.ID_YES:
+            if SaveATranslatorDictionary({}, self.pathTranslations):
+                dialog =  wx.MessageDialog(self, "Se reseteó el traductor", "Aviso al usuario", style = wx.OK|wx.ICON_INFORMATION|wx.STAY_ON_TOP)
+                dialog.ShowModal()
+            else:
+                dialog =  wx.MessageDialog(self, "No se pudo resetear el traductor. Verifique que el siguiente fichero está cerrado e intentelo de nuevo:\n{}".format(os.path.realpath(self.pathTranslations)), "Aviso al usuario", style = wx.OK|wx.ICON_INFORMATION|wx.STAY_ON_TOP)
+                dialog.ShowModal()
+
+    def CreateManualTranslator( self, event ):
+        dialog = wx.DirDialog(self, "¿Donde crear el traductor manual?", 'config/translation', style = wx.DD_DIR_MUST_EXIST | wx.DD_DEFAULT_STYLE)
+        if dialog.ShowModal() == wx.ID_OK:
+            path = dialog.GetPath()
+            txtDialog = wx.TextEntryDialog(self, "Por favor, indique el nombre del nuevo traductor Manual", 'Indique el nombre')
+            if txtDialog.ShowModal() == wx.ID_OK:
+                filename = txtDialog.GetValue().strip() +'.txt'
+                txtDialog.Destroy()
+                while os.path.exists(os.path.join(path, filename)) or filename=='.txt':
+                    txtDialog = wx.TextEntryDialog(self, "Nombre no es correcto. Ya existe o está vacio.\nPor favor, indique el nombre del nuevo traductor Manual", 'Indique el nombre')
+                    if txtDialog.ShowDialog() == wx.ID_OK:
+                        filename = txtDialog.GetValue().strip() +'.txt'
+                        txtDialog.Destroy()
+                    else: 
+                        filename = '.txt' #So the file creation is aborted
+                        break
+                if filename != '.txt': #The user introduced a valid name
+                    if SaveATranslatorDictionary({}, os.path.join(path, filename)):
+                        self.pathManualTranslator = os.path.join(path, filename)
+                        dialog =  wx.MessageDialog(self, "Se creó el traductor manual. La herramienta ha seleccionado el nuevo traductor como el traductor manual", "Aviso al usuario", style = wx.OK|wx.ICON_INFORMATION|wx.STAY_ON_TOP)
+                        dialog.ShowModal()
+
+            else:
+                txtDialog.Destroy()
+        dialog.Destroy()
+
+    def EditTranslatorManual( self, event ):
+        tf = TranslatorFrame(self)
+        tf.ConfigureForManual(self.pathManualTranslator)
+        self.Hide()
+        tf.Show()
 
     def startGeneration( self, event ):
         if not self.CheckValues():
@@ -223,7 +327,7 @@ class MainFrame ( wx.Frame ):
         i = 2
         for link in links:
             i += 1
-            err = worker.TranslateALink(link, self.dirPdfs)
+            err = worker.TranslateALink(link, self.dirPdfs.GetPath())
             if err != "":
                 dialog = wx.MessageDialog(self, "No se pudo generar la lista\nError: "+err+'\n¿Continuar?', style = wx.YES_NO|wx.ICON_ERROR|wx.STAY_ON_TOP)
                 if dialog.ShowModal() != wx.ID_YES:
@@ -239,14 +343,25 @@ class MainFrame ( wx.Frame ):
         progressDialog.Destroy()
         worker.DestroyWebDriver()
 
-        dialog = wx.MessageDialog(self, "Se ha finalizado la traducción de listas", style = wx.OK|wx.ICON_ERROR|wx.STAY_ON_TOP)    
+        dialog = wx.MessageDialog(self, "Se ha finalizado la traducción de listas", style = wx.OK|wx.ICON_INFORMATION|wx.STAY_ON_TOP)    
         dialog.ShowModal()
         
         news = worker.CheckNewTranslations()
         if news != {}:
-            dialog = wx.MessageDialog(self, "Se han traducido nuevas palabras, ¿desea guardarlas en el traductor?", style = wx.YES_NO|wx.ICON_ERROR|wx.STAY_ON_TOP)    
-            if dialog.ShowModal() == wx.ID_YES:
+            dialog = wx.MessageDialog(self, "Se han traducido nuevas palabras, ¿desea guardarlas en el traductor?", style = wx.YES_NO|wx.ICON_INFORMATION|wx.STAY_ON_TOP)
+            rsp = dialog.ShowModal()
+            if rsp == wx.ID_YES:
                 worker.SaveTranslations()
+            
+            elif rsp == wx.ID_NO:
+                dialog2 = wx.MessageDialog(self, "¿Quieres editar las palabras nuevas para guardarlas como traducciones manuales?", style = wx.YES_NO|wx.ICON_INFORMATION|wx.STAY_ON_TOP)
+                if dialog2.ShowModal() == wx.ID_YES:
+                    tf = TranslatorFrame(self)
+                    tf.ConfigureEditNewTranslations(self.pathManualTranslator, news)
+                    self.Hide()
+                    tf.Show()
+                dialog2.Destroy()
+            dialog.Destroy()
         
 
 
